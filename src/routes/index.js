6013ae3e251d53b9d2e1c93809f438794c4b12c3
Vaishnav <vaishnavvaichu123@gmail.com>
2022-12-18 00:02:23 +0530

    testing

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,28 +2,29 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "../presentation/Layout";
 import Home from "../presentation/Home";
-import Login from "../presentation/Login";
+import Login from "../components/Login";
 import SignUp from "../presentation/SignUp";
 import Testing from "../components/testing";
 
 export default function router() {
-  return (
-    <BrowserRouter>
-      {
-        <Routes>
-        <Route path="login" element={<Login />}></Route>
-                  
-          <Route path="/" element={<Layout />}>
-            <Route path="home" element={<Home />}></Route>
-            <Route path="testing" element={<Testing />}></Route>
+    return (
+        <BrowserRouter>
+            {
+                <Routes>
+                    <Route path="testing" element={<Testing />}></Route>
 
-            
-          </Route>
-          
-          <Route path="signup" element={<SignUp />}></Route>
-          <Route path="*" element={Login} />
-        </Routes>
-      }
-    </BrowserRouter>
-  );
+                    <Route path="login" element={<Login />}></Route>
+
+                    <Route path="/" element={<Layout />}>
+                        <Route path="home" element={<Home />}></Route>
+
+
+                    </Route>
+
+                    <Route path="signup" element={<SignUp />}></Route>
+                    <Route path="*" element={Login} />
+                </Routes>
+            }
+        </BrowserRouter>
+    );
 }
