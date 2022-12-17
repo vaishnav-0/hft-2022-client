cf964a52a020954a6e3ab3438e639ef0f778b7c5
naaji-navas <najidnavas2000@gamil.com>
2022-12-17 11:39:23 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { routes } from "./presentation/Common/Routes";
+import Login from "./presentation/Login"
+//component imports
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path={routes.LOGIN} component={Login} />
+      </Switch>
+    </Router>
   );
 }
 
