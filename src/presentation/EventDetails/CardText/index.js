7a88f1a0a27efbfb9518fa7212fc7e737781da37
naaji-navas <najidnavas2000@gamil.com>
2022-12-17 22:46:05 +0530

    added webcam

--- src/presentation/EventDetails/CardText/index.js
+++ src/presentation/EventDetails/CardText/index.js
@@ -1,11 +1,9 @@
-import React from "react";
 import { message } from "antd";
+import React from "react";
 const index = () => {
-    const handkeClick = () => {
-        message.success("You have successfully registered for the event");
-
-        
-    };
+  const handkeClick = () => {
+    message.success("You have successfully registered for the event");
+  };
   return (
     <div>
       <p class="mb-3 font-light text-dark-500">
@@ -21,10 +19,10 @@ const index = () => {
         eliminate toil, and deploy changes with ease, with a complete audit
         trail for every change.
       </p>
-          <button  
-              onClick={() => {
-                  handkeClick();
-              }}
+      <button
+        onClick={() => {
+          handkeClick();
+        }}
         type="button"
         class="   fixed bottom-12 w-[90%] text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-[2rem] text-sm px-5 py-2.5 text-center mr-2 mb-2"
       >
