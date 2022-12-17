653e28879a3fb77b5ef7c3aa7ca7f2c729c18524
Vaishnav <vaishnavvaichu123@gmail.com>
2022-12-18 00:08:38 +0530

    integration

--- src/presentation/Login/index.js
+++ src/presentation/Login/index.js
@@ -1,6 +1,38 @@
 import React from "react";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+
+  const navigate = useNavigate();
+  const provider = new GoogleAuthProvider();
+  provider.addScope("openid");
+  provider.addScope("profile");
+  provider.addScope("email");
+
+  const signIn = () =>
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        // The signed-in user info.
+        navigate("/");
+
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+
   return (
     <div>
       <div>
