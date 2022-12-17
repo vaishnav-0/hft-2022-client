c9799ab7c392ebce3b867db9f28441abda376c5c
Vaishnav <vaishnavvaichu123@gmail.com>
2022-12-18 00:40:59 +0530

    before merge

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,30 +5,33 @@ import Layout from "../presentation/Layout";
 import Login from "../presentation/Login";
 import EventDetails from "../presentation/EventDetails";
 import Profile from "../presentation/Profile";
-
+import { RequireAuth } from "./RequireAuth";
 import SignUp from "../presentation/SignUp";
 import Testing from "../components/testing";
+import { NotRequireAuth } from "./NotRequireAuth";
 
 export default function router() {
-  return (
-    <BrowserRouter>
-      {
-        <Routes>
-          <Route path="testing" element={<Testing />}></Route>
+    return (
+        <BrowserRouter>
+            {
+                <Routes>
+                    <Route path="testing" element={<Testing />}></Route>
 
-          <Route path="login" element={<Login />}></Route>
-          
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />}></Route>
-          </Route>
-                  <Route path="eventdetails" element={<EventDetails />}></Route>
-                  <Route path="profile" element={<Profile />}></Route>
-                  
+                    <Route path="login" element={<NotRequireAuth redirect="/"><Login /></NotRequireAuth>}></Route>
 
-          <Route path="signup" element={<SignUp />}></Route>
-          <Route path="*" element={Login} />
-        </Routes>
-      }
-    </BrowserRouter>
-  );
+                    <Route path="/" element={<RequireAuth redirect="/login"/>}>
+                        <Route path="/" element={<Layout /> }>
+                            <Route index element={<Home/>} />
+                        </Route>
+                    </Route>
+                    <Route path="eventdetails" element={<EventDetails />}></Route>
+                    <Route path="profile" element={<Profile />}></Route>
+
+
+                    <Route path="signup" element={<SignUp />}></Route>
+                    <Route path="*" element={Login} />
+                </Routes>
+            }
+        </BrowserRouter>
+    );
 }
