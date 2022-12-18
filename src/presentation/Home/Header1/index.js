f758b98d69b68acb848f4a79d3cee613b3e3505d
naaji-navas <najidnavas2000@gamil.com>
2022-12-18 08:01:43 +0530

    deleted some unwanted componets

--- src/presentation/Home/Header1/index.js
+++ src/presentation/Home/Header1/index.js
@@ -1,11 +1,10 @@
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import React from "react";
+import { auth } from "../../../firebase";
+import { likeEvent, unlikeEvent } from "../../../functions/db";
 import Celebrities from "../../Common/icons/Celebrities.svg";
 import Group from "../../Common/icons/Group.svg";
-import Heart from "../../Common/icons/heart.svg";
-import FavoriteIcon from '@mui/icons-material/Favorite';
 import "./style.css";
-import { likeEvent, unlikeEvent } from "../../../functions/db";
-import { auth } from "../../../firebase";
 
 function Index({ id, location, likes, userName, liked }) {
   const user = auth.currentUser;
@@ -17,30 +16,30 @@ function Index({ id, location, likes, userName, liked }) {
     if (!isLiking.current) {
       isLiking.current = true;
       if (!isLiked) {
-        likeEvent(id, user.uid).then(() => {
-          setIsLiked(true)
-          setLikeCount(prev => prev + 1);
-        }).finally(() => {
-          isLiking.current = false;
-
-        });
+        likeEvent(id, user.uid)
+          .then(() => {
+            setIsLiked(true);
+            setLikeCount((prev) => prev + 1);
+          })
+          .finally(() => {
+            isLiking.current = false;
+          });
       } else {
-        unlikeEvent(id, user.uid).then(() => {
-          setIsLiked(false);
-          setLikeCount(prev => prev - 1);
-
-        }).finally(() => {
-          isLiking.current = false;
-
-        });
+        unlikeEvent(id, user.uid)
+          .then(() => {
+            setIsLiked(false);
+            setLikeCount((prev) => prev - 1);
+          })
+          .finally(() => {
+            isLiking.current = false;
+          });
       }
     }
-
-  }
+  };
 
   React.useEffect(() => {
     setIsLiked(liked);
-  }, [liked])
+  }, [liked]);
   return (
     <div className="w-100 content-center justify-center ">
       <div id="base_card">
@@ -57,13 +56,16 @@ function Index({ id, location, likes, userName, liked }) {
             <div>
               <img src={Group} alt="another" />
               <p>{likeCount}</p>
-              <FavoriteIcon style={{ color: isLiked ? "red" : "white" }} onClick={like} />
+              <FavoriteIcon
+                style={{ color: isLiked ? "red" : "white" }}
+                onClick={like}
+              />
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Index;
